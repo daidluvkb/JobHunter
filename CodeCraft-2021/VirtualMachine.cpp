4b8b1872e3b317003b7dc0005a588bd1a0e4d0ee
daidluvkb <dai825408853@163.com>
2021-03-18 21:57:38 +0800

    modify double node logic err in vm.cpp

--- CodeCraft-2021/VirtualMachine.cpp
+++ CodeCraft-2021/VirtualMachine.cpp
@@ -5,9 +5,15 @@ using namespace std;
 VirtualMachine::VirtualMachine(string &type, int num_of_cpu, int size_of_mem, bool is_double_node, int index)
     : m_type(type),
       m_num_of_cpu(num_of_cpu), m_size_of_mem(size_of_mem),
-      m_is_double_node(is_double_node), m_index(index),
+      m_index(index),
       _host(nullptr)
 {
+    if (is_double_node)
+        m_is_double_node = 0;
+    else
+    {
+        m_is_double_node = -1;
+    }
 }
 
 int VirtualMachine::getId()
